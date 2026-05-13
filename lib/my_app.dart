5e422043dd353401359fa0a6e191ca953f8cef7d
mdrahib46 <nahid179846@bsdi-bd.org>
2026-05-13 10:39:29 +0600

    intro commit

---
+++ lib/my_app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:student_info/home_screen.dart';
+
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
